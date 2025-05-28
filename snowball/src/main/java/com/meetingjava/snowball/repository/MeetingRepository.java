cffcbad6266f6be09df65779a629d4d5f1ac4529
ssong721 <sminji721@gmail.com>
2025-05-28 17:39:21 +0900

    미팅 수정

--- snowball/src/main/java/com/meetingjava/snowball/repository/MeetingRepository.java
+++ snowball/src/main/java/com/meetingjava/snowball/repository/MeetingRepository.java
@@ -3,5 +3,5 @@ package com.meetingjava.snowball.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.meetingjava.snowball.entity.Meeting;
 
-public interface MeetingRepository extends JpaRepository<Meeting, Long> {
+public interface MeetingRepository extends JpaRepository<Meeting, String> {
 }
