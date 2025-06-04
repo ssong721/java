a3cb61868ffbfc16ff0947d91df7eed849783b51
ssong721 <sminji721@gmail.com>
2025-06-04 13:07:30 +0900

    홈화면에서 멤버 수 오류 수정

--- snowball/src/main/java/com/meetingjava/snowball/repository/MemberRepository.java
+++ snowball/src/main/java/com/meetingjava/snowball/repository/MemberRepository.java
@@ -11,4 +11,5 @@ import java.util.List;
 public interface MemberRepository extends JpaRepository<Member, String> {
     Optional<Member> findByUserAndMeeting(User user, Meeting meeting);
     List<Member> findByMeeting(Meeting meeting);
+    int countByMeeting(Meeting meeting);
 }
